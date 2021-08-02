618cff682458e9fcdd45e0feb337607d2f298241
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-02 12:59:55 +0100

    added firebase_core, versions updates etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:phnauthnew/services/authservice.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,4 +18,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-  
